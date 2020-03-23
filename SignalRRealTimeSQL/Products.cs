d12197f0e0ea7001d015555872580730041a9d2c
aji <ajidwip6@gmail.com>
2020-03-23 09:33:38 +0700

    first commit

---
+++ SignalRRealTimeSQL/Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SignalRRealTimeSQL
+{
+    public class Products
+    {
+        public string UserID { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string NIK { get; set; }
+    }
+}
---
+++ SignalRRealTimeSQL/index.aspx.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SignalRRealTimeSQL
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        [WebMethod]
+        public static IEnumerable<Products> GetData()
+        {
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DataBase"].ConnectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(@"SELECT [UserId],[UserName],[Email],[NIK] FROM [dbo].[T_MsUser]", connection))
+                {
+                    // Make sure the command object does not already have
+                    // a notification object associated with it.
+                    command.Notification = null;
+                    SqlDependency.Start(ConfigurationManager.ConnectionStrings["DataBase"].ConnectionString);
+                    SqlDependency dependency = new SqlDependency(command);
+                    dependency.OnChange += new OnChangeEventHandler(dependency_OnChange);
+
+                    if (connection.State == ConnectionState.Closed)
+                        connection.Open();
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    var listData = reader.Cast<IDataRecord>()
+                          .Select(x => new Products()
+                          {
+                              UserID = x.GetString(0),
+                              UserName = x.GetString(1),
+                              Email = x.GetString(2),
+                              NIK = x.GetString(3)
+                          }).ToList();
+
+                    return listData;
+                }
+            }
+        }
+        private static void dependency_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            MyHub.Show();
+        }
+
+    }
+}
